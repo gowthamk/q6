d8d1e8a38b8a63d30a0512e73c9ef88e308fe3c4
Kapil Subramaniam <earankyk@gmail.com>
2018-01-17 17:13:56 -0500

    Adding new microblog

--- examples/microblog_app.ml
+++ examples/microblog_app.ml
@@ -37,6 +37,13 @@ struct
     | [] -> true
     | x::xs -> (f x)&&(forall xs f)
 
+  let rev_list l =
+  let rec rev_acc acc = function
+    | [] -> acc
+    | hd::tl -> rev_acc (hd::acc) tl
+  in 
+  rev_acc [] l
+
   let rec exists l f = match l with
     | [] -> false
     | x::xs -> (f x)||(exists xs f)
@@ -52,10 +59,10 @@ module User =
 struct
   type id = Uuid.t
   type eff = Add of {username: string; pwd: string}
-    | AddFollowing of {leader_id: id}
-    | RemFollowing of {leader_id: id}
-    | AddFollower of {follower_id: id}
-    | RemFollower of {follower_id: id} 
+    | AddFollowing of {leader_id: id; timestamp: int}
+    | RemFollowing of {leader_id: id; timestamp: int}
+    | AddFollower of {follower_id: id; timestamp: int}
+    | RemFollower of {follower_id: id;timestamp: int} 
     | Blocks of {follower_id: id}
     | GetBlocks
     | IsBlockedBy of {leader_id: id}
@@ -118,7 +125,7 @@ end
  * There is a zip/map2 bug unfixed. Fix goes in mkfun 
  * inside rdtextract.ml
  *) 
-let do_test1 uid name = 
+(*let do_test1 uid name = 
   let x = [1;2] in
   let y = UserName.GetId in
   let z = UserName.Add {user_id=uid} in
@@ -131,14 +138,14 @@ let do_test1 uid name =
   let u4 = List.fold_right (fun idop acc -> match idop with
                              | Some uid' -> uid'::acc
                              | None -> acc) u3 [] in
-    u4
+    u4*)
 
-let do_add_user name pwd = 
+(*let do_add_user name pwd = 
   let uid = Uuid.create() in
   begin
     UserName_table.append name (UserName.Add {user_id=uid});
     User_table.append uid (User.Add {username=name;pwd=pwd})
-  end 
+  end*) 
 
 let get_user_id_by_name nm = 
   let ctxt = (* ea *) UserName_table.get nm (UserName.GetId) in
@@ -153,15 +160,35 @@ let get_user_id_by_name nm =
       List.first_some ids
     end
 
-let do_block_user me other  = 
+(*let do_block_user me other  = 
   let Some my_id = get_user_id_by_name me in
   let Some other_id = get_user_id_by_name other in
+  let ts = int_of_float (Unix.time ()) in
     begin
       User_table.append my_id (User.Blocks {follower_id=other_id});
       User_table.append other_id (User.IsBlockedBy {leader_id=my_id});
-      User_table.append my_id (User.RemFollower {follower_id=other_id}); 
-     User_table.append other_id (User.RemFollowing {leader_id=my_id})
-    end
+      User_table.append my_id (User.RemFollower {follower_id=other_id; timestamp=ts}); 
+      User_table.append other_id (User.RemFollowing {leader_id=my_id; timestamp=ts})
+    end*)
+
+let find_last_ts fid ctxt = 
+  let ts_list = List.map (fun eff -> match eff with
+                  | Some x -> (match x with 
+                              | User.AddFollower {follower_id=fid1; timestamp=ts1} -> if fid=fid1 then ts1 else (0-1) 
+                              | _ -> (match x with 
+                                     | User.RemFollower {follower_id=fid2; timestamp=ts2} -> if fid2=fid then ts2 else (0-1)
+                                     | _ -> (0-1)))
+                  | _ -> (0-1)) ctxt in
+  List.fold_left (fun acc ts -> if ts > acc then ts else acc) (0-1) ts_list
+  (*List.fold_right (fun ts acc -> if ts > acc then ts else acc) ts_list (0-1)*)
+
+let is_follower fid ctxt = 
+  let ts = find_last_ts fid ctxt in
+  List.fold_right (fun eff acc -> match eff with 
+                      | Some x -> (match x with
+                                    | User.AddFollower {follower_id=fid1; timestamp=ts1} -> fid1=fid && ts1=ts
+                                    | _ -> acc)
+                      | _ -> acc) ctxt false
 
 let do_new_tweet uid str = 
   let ctxt = User_table.get uid (User.GetFollowers) in
@@ -169,7 +196,7 @@ let do_new_tweet uid str =
                (fun eff -> match eff with 
                   | Some x -> 
                       (match x with 
-                         | User.AddFollower {follower_id=fid} -> Some fid
+                         | User.AddFollower {follower_id=fid; timestamp=ts} -> if (is_follower fid ctxt) then Some fid else None
                          | _ -> None)
                   | _ -> None) ctxt in
   let tweet_id = Uuid.create() in
@@ -191,20 +218,20 @@ let get_tweet tid =
   let res = List.first_some tweets in
     res
 
-let inv_fkey uid' = 
+let inv_fun uid' = 
   List.forall (Userline_table.get uid' (Userline.Get))
-    (fun e -> match e with
-       | Some x -> 
-           (match x with 
-              | Userline.NewTweet {tweet_id=tid} -> 
-                  List.exists (Tweet_table.get tid Tweet.Get) 
-                    (fun e' -> match e' with 
-                       | Some y -> (match y with 
-                                      | (Tweet.New _) -> true 
-                                      | _ -> false)
-                       | _ -> false)
-              | _ -> false)
-       | _ -> true)
+  (fun e -> match e with
+           | Some x -> 
+               (match x with 
+                  | Userline.NewTweet {tweet_id=tid} -> 
+                      List.exists (Tweet_table.get tid Tweet.Get) 
+                        (fun e' -> match e' with 
+                           | Some y -> (match y with 
+                                          | (Tweet.New _) -> true 
+                                          | _ -> false)
+                           | _ -> false)
+                  | _ -> false)
+           | _ -> true)
 
 let get_userline uid = 
   let ctxt = Userline_table.get uid (Userline.Get) in
