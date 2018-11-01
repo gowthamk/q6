287fe31f2d2d87e26eccb49269d81f8071597971
gowthamk <gowtham@purdue.edu>
2018-11-01 12:05:18 -0400

    CRDT-style examples added.

---
+++ examples/crdt_ba.ml
@@ -0,0 +1,46 @@
+open Crdts
+
+module BankAccount_Types = struct
+  type account = {id:Uuid.t; name:string; mutable bal:CRInt.t}
+  type accounts_table =  account CRTable.t
+  type ba_db = {accounts_table: accounts_table}
+end
+
+open BankAccount_Types
+
+module BankAccounts(S:sig
+                        val t: accounts_table
+                      end) = struct
+  open S
+
+  let get_balance (acc_id:Uuid.t) = 
+    let b = CRTable.find (fun {bal} -> bal) 
+              (fun {id} -> id = acc_id) t in
+    match b with | Some x -> CRInt.get x
+                 | None -> 0
+
+  let deposit (acc_id:Uuid.t) amt = 
+    CRTable.update (fun {bal} -> CRInt.add amt bal)  
+      (fun {id} -> id = acc_id) t 
+
+  let withdraw (acc_id:Uuid.t) amt = 
+    if get_balance acc_id >= amt then
+      begin
+        CRTable.update (fun {bal} -> CRInt.add (0-amt) bal)
+          (fun {id} -> id = acc_id) t;
+        true
+      end
+    else false
+end
+
+module BankingApp(S:sig 
+                      val db: ba_db 
+                    end) = struct
+  module BA = BankAccounts(struct 
+                            let t = S.db.accounts_table
+                           end)
+  open BA
+  let transfer_txn (acc_id1:Uuid.t) (acc_id2:Uuid.t) amt = 
+    let status = withdraw acc_id1 amt in 
+    if status then deposit acc_id2 amt else ()
+end
---
+++ examples/crdt_mb.ml
@@ -0,0 +1,88 @@
+open Crdts
+
+module Microblog_Types = struct
+  type user = {id:Uuid.t; name:string; mutable followers:Uuid.t CRSet.t}
+  type users_table =  user CRTable.t
+  type tweet = {id:Uuid.t; author_id: Uuid.t; content: string}
+  type tweets_table = tweet CRTable.t
+  type line_item = {user_id:Uuid.t; tweet_id:Uuid.t}
+  type userline_table = line_item CRTable.t
+  type timeline_table = line_item CRTable.t
+  type mb_db = {users_table:users_table; 
+                tweets_table: tweets_table; 
+                userline_table: userline_table;
+                timeline_table: timeline_table}
+end
+
+open Microblog_Types
+
+module User(S:sig 
+                 val t: users_table 
+               end) = struct
+  open S
+
+  let add_follower (user_id:Uuid.t) (follower_id: Uuid.t) =
+    CRTable.update 
+      (fun {followers} -> CRSet.add follower_id followers) 
+      (fun {id} -> id=user_id) t
+
+  let remove_follower (user_id:Uuid.t) (follower_id: Uuid.t) =
+    CRTable.update 
+      (fun {followers} -> CRSet.remove follower_id followers) 
+      (fun {id} -> id=user_id) t
+
+  let get_followers (user_id:Uuid.t) : Uuid.t list = 
+    let fols = CRTable.find (fun {followers} -> followers) 
+                (fun {id} -> id = user_id) t in
+    match fols with | Some fs -> CRSet.get fs
+                    | None -> []
+end
+
+
+module Userline(S:sig 
+                    val t: userline_table 
+                  end) = struct
+  open S
+
+  let add (user_id:Uuid.t) (tweet_id:Uuid.t) = 
+    CRTable.insert {user_id=user_id; tweet_id=tweet_id} t
+end
+
+module Timeline(S:sig 
+                    val t: timeline_table 
+                  end) = struct
+  open S
+
+  let add (user_id:Uuid.t) (tweet_id:Uuid.t) = 
+    CRTable.insert {user_id=user_id; tweet_id=tweet_id} t
+end
+
+module Tweet(S:sig
+                val t: tweets_table
+               end) = struct
+  open S
+
+  let add tweet_id author_id content = 
+    CRTable.insert {id=tweet_id; author_id=author_id; 
+                    content=content} t
+end
+     
+module MicroblogApp(S:sig 
+                        val db: mb_db 
+                      end) = struct
+  open S
+  module User = User(struct let t = db.users_table end)
+  module Userline = Userline(struct let t = db.userline_table end)
+  module Timeline = Timeline(struct let t = db.userline_table end)
+  module Tweet = Tweet(struct let t = db.tweets_table end)
+
+  let do_new_tweet (uid:Uuid.t) (str:string) = 
+    let tweet_id = Uuid.create () in
+    let fols = User.get_followers uid in
+    begin
+      Tweet.add tweet_id uid str;
+      Userline.add uid tweet_id;
+      List.iter (fun fid -> Timeline.add fid tweet_id) fols;
+    end
+
+end
