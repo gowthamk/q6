aa01c1eea8eda3da9ad08929cf1ae0b8fdc28c95
gowthamk <gowtham@purdue.edu>
2018-11-01 21:01:54 -0400

    One more...

---
+++ examples/crdts.ml
@@ -0,0 +1,23 @@
+module CRInt = struct
+  type t = int
+
+  let get t = t
+  let add a t = fun t' -> t'+a
+end
+
+module CRTable = struct
+  type 'a t = 'a list
+
+  let find f g t = failwith "Unimpl."
+  let update f g t = ()
+  let insert x t = ()
+end
+
+module CRSet = struct
+  type 'a t = 'a list
+
+  let add x t = ()
+  let remove x t = ()
+  let get t = t
+end
+
